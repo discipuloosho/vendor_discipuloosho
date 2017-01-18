1767368d662cbe96927219e5c60a4544814ff83a
Antonio Jesús <ajmtrz@gmail.com>
2017-01-18 14:17:33 +0100

    Fuck OpenGapps

--- config/common.mk
+++ config/common.mk
@@ -1,29 +1,3 @@
-# OpenGapps
-GAPPS_FORCE_PACKAGE_OVERRIDES := true
-GAPPS_FORCE_WEBVIEW_OVERRIDES := true
-GAPPS_FORCE_MMS_OVERRIDES := true
-GAPPS_FORCE_DIALER_OVERRIDES := true
-GAPPS_FORCE_BROWSER_OVERRIDES := true
-PRODUCT_PACKAGES += \
-    CalculatorGoogle \
-    CalendarGooglePrebuilt \
-    CloudPrint2 \
-    Drive \
-    GCS \
-    GoogleCamera \
-    GoogleContacts \
-    Maps \
-    Photos \
-    PlayGames \
-    PrebuiltGmail \
-    PrebuiltExchange3Google \
-    PrebuiltKeep \
-    Street \
-    TranslatePrebuilt \
-    Velvet \
-    Wallet \
-    YouTube
-
 # Custom packages
 PRODUCT_PACKAGES += \
     WakeGestures \
