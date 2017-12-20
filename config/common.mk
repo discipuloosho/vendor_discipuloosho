6f6c0e125994da4982eccd4989280d8713dd3c6f
discipuloosho <ajmtrz@gmail.com>
2017-12-20 23:37:35 +0100

    nethunter: removed

--- config/common.mk
+++ config/common.mk
@@ -1,11 +1,5 @@
 # Custom packages
 PRODUCT_PACKAGES += \
-    WakeGestures \
     KernelAdiutor \
     WundergroundWeatherProvider \
-    CMFileManager \
-    cSploit \
-    OpenVPN \
-    RFAnalyzer \
-    Shodan \
-    USBKeyboard
+    CMFileManager
